cbb5a8cf9c9445f0a34774025588fc9d3631fef3
Alice Chang <sinsofangels@gmail.com>
2020-12-21 22:43:08 -0500

    Set up database migrations

--- data/migrations/20201221220559_plants.db3.js
+++ data/migrations/20201221220559_plants.db3.js
@@ -1,8 +1,25 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", col => {
+      col.increments();
+      col.text("username", 128).notNullable().unique();
+      col.text("password", 18).notNullable();
+      col.text("telephone", 128).notNullable();
+    })
+    .createTable("plants", col => {
+      col.increments();
+      col.text("nickname", 128).notNullable().defaultTo("unknown");
+      col.text("species", 128).notNullable().defaultTo("unknown");
+      col.integer("frequency").notNullable().defaultTo(24).unsigned();
+      col.text("img_url");
+      col.integer("user_id").notNullable().unsigned()
+        .references("id").inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('table'); 
 };
